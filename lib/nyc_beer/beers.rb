faab6375bd36570e9c1241817fdac33472c5143c
Zachary Lehmann <zlehmann@gmail.com>
2018-10-18 02:20:43 +0000

    fleshing out object abstraction for classes

--- lib/nyc_beer/beers.rb
+++ lib/nyc_beer/beers.rb
@@ -1,3 +1,14 @@
 class Beer
+  attr_accessor :name, :style, :abv, :ratings, :score
+  @@all = []
+
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 
 end
--- lib/nyc_beer/breweries.rb
+++ lib/nyc_beer/breweries.rb
@@ -1,3 +1,14 @@
 class Brewery
+  attr_accessor :name, :address
+  @@all = []
 
+  def initialize(name, address)
+    @name = name
+    @address = address
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
